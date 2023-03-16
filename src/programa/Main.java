d0f820115139b197e62f74dd9a951f93ce10eb69
Matias Bernal <matrekb@gmail.com>
2023-03-16 06:52:17 -0300

    more examples

--- src/programa/Main.java
+++ src/programa/Main.java
@@ -14,12 +14,18 @@ public class Main {
 
         //Se crea un profesor
         Profesor gamalielQuiroz = new Profesor("Gamaliel Natanael","Quiroz", 24, Informatica, 1, 5, 2, "A");
+        Profesor pepitoQuiroga = new Profesor("Pepito", "Quiroga", 46, Arte, 1, 8, 6, "B");
 
         //Se crea un alumno
         Alumno matiasBernal = new Alumno("Matias", "Bernal", 22, 1505, 2, "A");
+        Alumno fulanoRodriguez = new Alumno("Fulano", "Rodriguez", 24, 5051, 6, "B");
 
         //Probando mensajes
         JOptionPane.showMessageDialog(null, matiasBernal.setMateria(Informatica));
         JOptionPane.showMessageDialog(null, matiasBernal);
+        JOptionPane.showMessageDialog(null, fulanoRodriguez.setMateria(Arte));
+        JOptionPane.showMessageDialog(null, fulanoRodriguez);
+        JOptionPane.showMessageDialog(null, pepitoQuiroga);
+        JOptionPane.showMessageDialog(null, gamalielQuiroz);
     }
 }
