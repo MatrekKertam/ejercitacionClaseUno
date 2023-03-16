13d06b3777b116acb01d9124c93f2ffa86d1871c
Matias Bernal <matrekb@gmail.com>
2023-03-16 06:29:57 -0300

    Main testing

--- src/programa/Alumno.java
+++ src/programa/Alumno.java
@@ -5,6 +5,7 @@ public class Alumno extends Persona{
     private int matricula;
     private int año; //Acá sería el año en el que esta, ej: 6 (sexto)
     private String curso; //Acá el curso, ej: A
+    private Materia materia; //Agregamos alguna materia en la que participa el alumno
 
 
     public Alumno(String nombre, String apellido, int edad, int matricula, int año, String curso) {
@@ -36,5 +37,14 @@ public class Alumno extends Persona{
 
     public void setCurso(String curso) {
         this.curso = curso;
+    }
+
+    public Materia getMateria(){
+        return materia;
+    }
+
+    public String setMateria(Materia materia){
+        this.materia = materia;
+        return "Se inscribio al alumno " + this.getNombre() +" " + this.getApellido() + " en la materia: " + this.getMateria();
     }
 }
--- src/programa/Main.java
+++ src/programa/Main.java
@@ -1,5 +1,7 @@
 package programa;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -7,10 +9,13 @@ public class Main {
         Materia Politica = new Materia("Politica");
         Materia educacionFisica = new Materia("Eduacion Fisica");
         Materia Arte = new Materia("Arte");
-        Materia Informatica = new Materia("Informática");
+        Materia Informatica = new Materia("Informatica");
 
         Profesor gamalielQuiroz = new Profesor("Gamaliel Natanael","Quiroz", 24, Informatica, 1, 5, 2, "A");
 
-        Alumno matiasBernal = new Alumno("Matías", "Bernal", 22, 1505, 2, "A");
+        Alumno matiasBernal = new Alumno("Matias", "Bernal", 22, 1505, 2, "A");
+
+        System.out.println(matiasBernal.setMateria(Informatica));
+        JOptionPane.showMessageDialog(null, matiasBernal.setMateria(Informatica));
     }
 }
